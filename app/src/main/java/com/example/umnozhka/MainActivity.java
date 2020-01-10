f27aba84829a9ab04213eadcc1b4bdfdbb74498a
kamar19 <kaimar@mail.ru>
2020-01-10 17:50:12 +0600

    30dec

--- app/src/main/java/com/example/umnozhka/MainActivity.java
+++ app/src/main/java/com/example/umnozhka/MainActivity.java
@@ -18,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     SharedPreferences sharedPreferences;
     private static final String PREFERENCES_SETTINGS_NAME = "umnozhka_Settings";
+    private static String PREFERENCES_SETTINGS_HEARTSLIVECOUNT = "heartLiveCount";
 //    CheckBoxPreference chb3;
 //    PreferenceCategory categ2;
 
@@ -43,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static boolean SETTINGS_RECORD;            // На выживание (на рекорд)
     private static  int SETTINGS_TIME_TASK;         // Время на одну задачу
     private static  int SETTINGS_TIME_SESSION;      // Время на один сеанс, после уменьшается
+    private static  int heartLiveCount;
 
     Button buttonDigit1,buttonDigit2,buttonDigit3,buttonDigit4,buttonDigit5,buttonDigit6,buttonDigit7,buttonDigit8,buttonDigit9,
             buttonDigit0,buttonEnter, buttonBackSpace;
@@ -147,7 +149,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 */
             LoadPreferences();
         } else {
+            if (sharedPreferences.contains(PREFERENCES_SETTINGS_HEARTSLIVECOUNT)) {
+               // Получаем число из настроек
+                heartLiveCount = sharedPreferences.getInt(PREFERENCES_SETTINGS_HEARTSLIVECOUNT, 0);
+                }
 
+        }
 
 //            Toast toast = Toast.makeText(getApplicationContext(),
 //                    "Настройки Загружены!", Toast.LENGTH_SHORT);
@@ -180,7 +187,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //            SavePreferences();
 
         }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Запоминаем данные
+        SharedPreferences.Editor editorSharedPreferences = sharedPreferences.edit();
+        editorSharedPreferences.putInt(PREFERENCES_SETTINGS_HEARTSLIVECOUNT, heartLiveCount);
+        editorSharedPreferences.apply();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 //        MenuItem mi = menu.add(0, 1, 0, R.string.menu_Preferences);
@@ -282,6 +299,205 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 */
+private void RefrishIconLive(){
+    View view1,view2,view3,view4,view5;
+    view1 = findViewById(R.id.imageView1);
+    view2 = findViewById(R.id.imageView2);
+    view3 = findViewById(R.id.imageView3);
+    view4 = findViewById(R.id.imageView4);
+    view5 = findViewById(R.id.imageView5);
+    if (heartLiveCount>5) heartLiveCount=5;
+    if (heartLiveCount<0) heartLiveCount=0;
+
+
+
+    switch (heartLiveCount){
+        case 0:
+            view1.setVisibility( View.INVISIBLE);
+            view2.setVisibility( View.INVISIBLE);
+            view3.setVisibility( View.INVISIBLE);
+            view4.setVisibility( View.INVISIBLE);
+            view5.setVisibility( View.INVISIBLE);
+            break;
+        case 1:
+            view1.setVisibility( View.VISIBLE);
+            view2.setVisibility( View.INVISIBLE);
+            view3.setVisibility( View.INVISIBLE);
+            view4.setVisibility( View.INVISIBLE);
+            view5.setVisibility( View.INVISIBLE);
+            break;
+        case 2:
+            view1.setVisibility( View.VISIBLE);
+            view2.setVisibility( View.VISIBLE);
+            view3.setVisibility( View.INVISIBLE);
+            view4.setVisibility( View.INVISIBLE);
+            view5.setVisibility( View.INVISIBLE);
+            break;
+        case 3:
+            view1.setVisibility( View.VISIBLE);
+            view2.setVisibility( View.VISIBLE);
+            view3.setVisibility( View.VISIBLE);
+            view4.setVisibility( View.INVISIBLE);
+            view5.setVisibility( View.INVISIBLE);
+            break;
+        case 4:
+            view1.setVisibility( View.VISIBLE);
+            view2.setVisibility( View.VISIBLE);
+            view3.setVisibility( View.VISIBLE);
+            view4.setVisibility( View.VISIBLE);
+            view5.setVisibility( View.INVISIBLE);
+            break;
+        case 5:
+            view1.setVisibility( View.VISIBLE);
+            view2.setVisibility( View.VISIBLE);
+            view3.setVisibility( View.VISIBLE);
+            view4.setVisibility( View.VISIBLE);
+            view5.setVisibility( View.VISIBLE);
+            break;
+    }
+}
+private void RefrishDate(){
+// Определяем действие
+// MULTIPLY - Deistvie = 1;
+// DIVIDE   - Deistvie = 2;
+// ADD      - Deistvie = 3;
+// SUBTRAC  - Deistvie = 4;
+
+//    double Deistvie = Math.random()*3;
+
+//    int Deistvie;
+    int Variants=0;
+    int Deistvie;
+
+    if (SETTINGS_MULTIPLY) Variants++;
+    if (SETTINGS_DIVIDE) Variants++;
+    if (SETTINGS_ADD) Variants++;
+    if (SETTINGS_SUBTRAC) Variants++;
+    switch (Variants){
+        case 1:
+            if ((SETTINGS_MULTIPLY)&& (!SETTINGS_DIVIDE)&&(!SETTINGS_ADD)&&(!SETTINGS_SUBTRAC)) { Deistvie=1; }
+            else if ((!SETTINGS_MULTIPLY)&& (SETTINGS_DIVIDE)&&(!SETTINGS_ADD)&&(!SETTINGS_SUBTRAC)) { Deistvie=2; }
+            else if ((!SETTINGS_MULTIPLY)&& (!SETTINGS_DIVIDE)&&(SETTINGS_ADD)&&(!SETTINGS_SUBTRAC)) { Deistvie=3; }
+            else if ((!SETTINGS_MULTIPLY)&& (!SETTINGS_DIVIDE)&&(!SETTINGS_ADD)&&(SETTINGS_SUBTRAC)) { Deistvie=4; };
+            break;
+        case 2:
+            Deistvie = (int) Math.random()*2;
+            if (Deistvie==1) {
+                if (SETTINGS_MULTIPLY) {
+                    Deistvie = 1;
+                } else if (SETTINGS_DIVIDE) {
+                    Deistvie = 2;
+                } else
+                    Deistvie = 3;
+                }
+            if (Deistvie==2) {
+                if (SETTINGS_SUBTRAC) {
+                    Deistvie = 4;
+                } else if (SETTINGS_ADD) {
+                    Deistvie = 3;
+                } else
+                    Deistvie = 2;
+            }
+            break;
+        case 3:
+            if ((SETTINGS_MULTIPLY)&&
+
+    }
+
+    if (Variants==1)
+
+
+
+
+        && (!SETTINGS_DIVIDE)&&(!SETTINGS_ADD)&&(!SETTINGS_SUBTRAC)) { Deistvie=1; }
+
+
+    if ((SETTINGS_MULTIPLY)&& (!SETTINGS_DIVIDE)&&(!SETTINGS_ADD)&&(!SETTINGS_SUBTRAC)) { Deistvie=1; }
+    if (s1='Y') and (s2='N') and (s3='Y') and (s4='N')  then
+            begin
+    Deistvie:=Random(2)+1;
+    if Deistvie=2 then Deistvie:=3;
+    end;
+// Только сложение и деление
+    if (s1='Y') and (s2='N') and (s3='N') and (s4='Y')  then
+            begin
+    Deistvie:=Random(2)+1;
+    if Deistvie=2 then Deistvie:=4;
+    end;
+// Только вычитание и умножение
+    if (s1='N') and (s2='Y') and (s3='Y') and (s4='N')  then
+            begin
+    Deistvie:=Random(2)+1;
+    if Deistvie=2 then Deistvie:=3;
+    if Deistvie=1 then Deistvie:=2;
+    end;
+// Только вычитание и деление
+    if (s1='N') and (s2='Y') and (s3='N') and (s4='Y')  then
+            begin
+    Deistvie:=Random(2)+1;
+    if Deistvie=2 then Deistvie:=4;
+    if Deistvie=1 then Deistvie:=2;
+    end;
+// Только умножение и деление
+    if (s1='N') and (s2='N') and (s3='Y') and (s4='Y')  then Deistvie:=Random(2)+3;
+
+// Сложение и вычитание, умножение
+    if (s1='Y') and (s2='Y') and (s3='Y') and (s4='N')  then Deistvie:=Random(3)+1;
+// Сложение и вычитание, деление
+    if (s1='Y') and (s2='Y') and (s3='N') and (s4='Y')  then
+            begin
+    Deistvie:=Random(3)+1;
+    if Deistvie=3 then Deistvie:=4;
+    end;
+// Сложение, умножение , деление
+    if (s1='Y') and (s2='N') and (s3='Y') and (s4='Y')  then
+            begin
+    Deistvie:=Random(3)+1;
+    if Deistvie=3 then Deistvie:=4;
+    if Deistvie=2 then Deistvie:=3;
+    end;
+// Вычитание, умножение, деление,
+    if (s1='N') and (s2='Y') and (s3='Y') and (s4='Y')  then Deistvie:=Random(3)+2;
+// Все действия
+    if (s1='Y') and (s2='Y') and (s3='Y') and (s4='Y')  then Deistvie:=Random(4)+1;
+// Определяем значения выражения
+// s5  Начало диапо
+// s6  Конец диапоз
+    Randomize;
+    Number1 := StrToInt(s5)+Random(StrToInt(s6)-StrToInt(s5));      //0..9, 10..50 = 10-0, 50-10=40
+    Number2 := StrToInt(s5)+Random(StrToInt(s6)-StrToInt(s5));      //0..9, 10..50 = 10-0, 50-10=40
+    if Deistvie=1 then Form1.label2.Caption:='+';
+    if Deistvie=2 then
+            begin
+    Form1.label2.Caption:='-';
+    // Если вычитание то Number1>Number2
+    if Number1<Number2 then
+    begin
+    TempN:=Number1;
+    Number1:=Number2;
+    Number2:=TempN;
+    end;
+    end;
+    if Deistvie=3 then Form1.label2.Caption:='*';
+    if Deistvie=4 then
+            // ищем только целые деления.
+            begin
+    Form1.label2.Caption:='÷';
+    if Number1<Number2 then
+    begin
+    TempN:=Number1;
+    Number1:=Number2;
+    Number2:=TempN;
+    end; // 20/6=3
+    TempN:= Number1 div Number2;
+    Number1:=Number2*TempN;
+    end;
+    Form1.Label1.Caption:=IntToStr(Number1);
+    Form1.Label3.Caption:=IntToStr(Number2);
+
+    end;
+
+}
 
     private void LoadPreferences() {
         // Думаю, что не нужно устанавливать в ручную переключатели и другие элементы в Preference Активности
